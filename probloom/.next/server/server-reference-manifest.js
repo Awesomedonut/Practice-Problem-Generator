24c636bbeb8fdb78c4dcbee9fc391b39e3080b37
Awesomedonut <yjs2@sfu.ca>
2023-11-15 13:21:39 -0800

    minor config modifications

--- probloom/.next/server/server-reference-manifest.js
+++ probloom/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"iEwc2nMzG324Y5uGYlrl5ks3CuXf3Ej3JBVRRDlnl0k=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"rFt5Jt5neNKiD8y/27GRlDSlBrOJgb6VUJlTL/WWtmU=\"\n}"
