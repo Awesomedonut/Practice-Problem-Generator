11a54f5ef2c2c5df9dd48e541a6a5acc442d706b
Julie Wu <julie.wuu123@gmail.com>
2023-11-16 19:00:13 -0800

    Updated About page. Looks better now

--- probloom/src/app/About/page.tsx
+++ probloom/src/app/About/page.tsx
@@ -20,7 +20,16 @@ const MyPage = () => {
       </Head>
 
       <div className={styles.header}>Probloom</div>
+      <div className={styles.header2}>About</div>
 
+      <div className={styles.imageContainer1}>
+        <img src="/about-flowers.png" alt="" />
+      </div>
+
+      <div className={styles.imageContainer2}>
+        <img src="/about-flowers.png" alt="" />
+      </div>
+  
       <div className={styles.mainContent}>
         <div className={styles.centeredBox}>
           <p>Lorem, isum dolor sit amet consectetur adipisicing elit. Ex aut officiis, voluptates ipsa laborum et maiores earum perspiciatis quae consequatur ducimus accusantium molestiae, vero excepturi fugit porro dicta facere repudiandae pariatur modi! Ullam sed ducimus, aliquam numquam tempore, nemo quam rem ipsum nisi doloremque totam modi non autem. Libero nulla alias tempore expedita fugiat, dignissimos excepturi itaque provident sequi. Laborum iusto in animi quia quidem, magnam dicta dolor perferendis dolore laboriosam, corporis fuga, ea quibusdam ipsa odit facilis eligendi. Quod doloremque voluptatum voluptatem? Tempora harum voluptas dicta nisi, eos sunt voluptatibus pariatur officiis culpa ea magnam aperiam sequi optio nesciunt, dolore libero voluptatem inventore laboriosam tempore id iste accusamus. Commodi sint voluptatem velit laboriosam repellat, in iusto ea quas vitae voluptatibus perspiciatis similique ipsam enim nam cupiditate deserunt praesentium nobis, quis vero tempora explicabo pariatur eligendi consequuntur necessitatibus? Expedita placeat sunt praesentium dignissimos doloribus laborum cupiditate asperiores? Aliquid quam distinctio eum unde eos enim aliquam inventore, doloribus dignissimos autem facilis quod magni nulla exercitationem. Harum deserunt cumque incidunt itaque quo beatae eligendi debitis et ex sed. Quos accusamus facilis sapiente consectetur! Optio officia itaque veniam quisquam corporis deleniti ut animi voluptatem, distinctio consectetur non aut eaque est fugiat voluptate expedita.</p>
