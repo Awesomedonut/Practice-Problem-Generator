820b11c6838283d1960545acf9add2e69e29bda8
Awesomedonut <Jessicayjsong@gmail.com>
2023-11-22 16:23:16 -0800

    fix compliation errors

--- probloom/src/app/components/Quiz/QuizSection.tsx
+++ probloom/src/app/components/Quiz/QuizSection.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Problem } from '../../types/Problem';
 import styles from './styles.module.css'
+
 type QuizSectionProps = {
     problems: Problem[];
     userAnswers: string[];
     handleAnswerChange: (index: number, value: string) => void;
-  };
-  
-  const QuizSection: React.FC<QuizSectionProps> = ({ problems, userAnswers, handleAnswerChange }) => {
+};
+
+const QuizSection: React.FC<QuizSectionProps> = ({ problems, userAnswers, handleAnswerChange }) => {
     return (      
       <div className={styles.quizSection}>
         <h1>Quiz</h1>
         {problems.map((problem, index) => (
           <div key={index} className={styles.questionSection}>
-            <p  className={styles.question}>Q{index+1}. {problem.question}</p>
+            <p className={styles.question}>Q{index+1}. {problem.question}</p>
+            <div className={styles.questionbody}>
             {problem.questionType === 'text' ? (
               <input
                 type="text"
@@ -22,24 +24,25 @@ type QuizSectionProps = {
               />
             ) : (
               problem.choices.map((choice, choiceIndex) => (
-                <div className={styles.choice}>
-                <label key={choiceIndex}>
-                  <input
-                    type="radio"
-                    name={`question${index}`}
-                    value={choice}
-                    checked={userAnswers[index] === choice}
-                    onChange={(e) => handleAnswerChange(index, e.target.value)}
-                  />
-                  {choice}
-                </label>
+                <div key={choiceIndex} className={styles.choice}>
+                  <label>
+                    <input
+                      type="radio"
+                      name={`question${index}`}
+                      value={choice}
+                      checked={userAnswers[index] === choice}
+                      onChange={(e) => handleAnswerChange(index, e.target.value)}
+                    />
+                    {choice}
+                  </label>
                 </div>
               ))
-            )}
+            )}</div>
+            
           </div>
         ))}
       </div>
     );
-  };
-  
-  export default QuizSection;
+};
+
+export default QuizSection;
