691c666de25ee6d81aa97fa9a034893b6aea81de
Zoe Wong <zoelevelup@gmail.com>
2023-11-24 23:56:13 -0800

    made input go to next line once reach max-width

--- probloom/.next/server/server-reference-manifest.js
+++ probloom/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"5z9IH2AX8KP3FjwZ++1K3jf7ugN3v8iHa8nLE0sFhRg=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"YdPCsZxKcnCKHq+eJ0xb5BK/nH2asSMHRUsmNqSM4hw=\"\n}"
--- probloom/src/app/components/Quiz/QuizSection.tsx
+++ probloom/src/app/components/Quiz/QuizSection.tsx
@@ -9,41 +9,53 @@ type QuizSectionProps = {
 };
 
 const QuizSection: React.FC<QuizSectionProps> = ({ problems, userAnswers, handleAnswerChange }) => {
-    return (      
-      <div className={styles.quizSection}>
-        <h1>Quiz</h1>
-        {problems.map((problem, index) => (
-          <div key={index} className={styles.questionSection}>
-            <p className={styles.question}>Q{index+1}. {problem.question}</p>
-            <div className={styles.questionbody}>
+  return (      
+    <div className={styles.quizSection}>
+      <h1>Quiz</h1>
+      {problems.map((problem, index) => (
+        <div key={index} className={styles.questionSection}>
+          <p className={styles.question}>Q{index+1}. {problem.question}</p>
+          <div className={styles.questionbody}>
             {problem.questionType === 'text' ? (
-              <input
-                type="text"
+              <textarea
+                id={`textQuestion${index}`}
+                name={`textQuestion${index}`}
+                rows={5}
+                cols={50}
+                style={{ width: '70%', padding: '10px', borderRadius: '16px', backgroundColor: 'rgb(56,56,56)', borderColor: 'rgb(0,0,0)',resize: 'none',}}
                 value={userAnswers[index] || ''}
                 onChange={(e) => handleAnswerChange(index, e.target.value)}
-                style={{ width: '70%', padding: '10px', borderRadius: '16px'}}
               />
             ) : (
-              problem.choices.map((choice, choiceIndex) => (
-                <div key={choiceIndex} className={styles.choice}>
-                  <label>
-                    <input
-                      type="radio"
-                      name={`question${index}`}
-                      value={choice}
-                      checked={userAnswers[index] === choice}
-                      onChange={(e) => handleAnswerChange(index, e.target.value)}
-                    />
-                    {choice}
-                  </label>
-                </div>
-              ))
-            )}</div>
-            
+              problem.questionType === 'multipleChoice' ? (
+                problem.choices.map((choice, choiceIndex) => (
+                  <div key={choiceIndex} className={styles.choice}>
+                    <label>
+                      <input
+                        type="radio"
+                        name={`question${index}`}
+                        value={choice}
+                        checked={userAnswers[index] === choice}
+                        onChange={(e) => handleAnswerChange(index, e.target.value)}
+                      />
+                      {choice}
+                    </label>
+                  </div>
+                ))
+              ) : (
+                <input
+                  type="text"
+                  value={userAnswers[index] || ''}
+                  onChange={(e) => handleAnswerChange(index, e.target.value)}
+                />
+              )
+            )}
           </div>
-        ))}
-      </div>
-    );
+        </div>
+      ))}
+    </div>
+  );
 };
+
 
 export default QuizSection;
